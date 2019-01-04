0f6526b0b95be51351249b33444f8b7c4fb5796a
Oliver Haase <oliver.koeln@gmail.com>
2019-01-04 14:01:32 +0100

    removed licence text

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,9 @@
-/**
- * @license
- * Copyright Akveo. All Rights Reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- */
 import { APP_BASE_HREF } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxLoadingModule } from 'ngx-loading';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -25,7 +21,8 @@ import { ThemeModule } from './theme/theme.module';
 
     CoreModule.forRoot(),
     ThemeModule.forRoot(),
-    ROSModule.forRoot({ url: 'ws://192.168.225.226:9090' })
+    ROSModule.forRoot({ url: 'ws://192.168.0.141:9090' }),
+    NgxLoadingModule.forRoot({})
   ],
   bootstrap: [AppComponent],
   providers: [
