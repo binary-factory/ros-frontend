ca2f2fe1d22396470f188df00d25fb9bc8bc30d9
Oliver Haase <oliver.koeln@gmail.com>
2019-01-05 19:42:06 +0100

    beta of webrtc

--- src/app/user-control-panel/pages/dashboard-page/security-cameras/security-cameras.component.ts
+++ src/app/user-control-panel/pages/dashboard-page/security-cameras/security-cameras.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import SimpleWebRTC from 'simplewebrtc';
 
 @Component({
   selector: 'ngx-security-cameras',
   styleUrls: ['./security-cameras.component.scss'],
   templateUrl: './security-cameras.component.html'
 })
-export class SecurityCamerasComponent {
+export class SecurityCamerasComponent implements AfterViewInit {
 
   cameras: any[] = [{
     title: 'Gegenüber',
@@ -22,5 +23,22 @@ export class SecurityCamerasComponent {
   selectCamera(camera: any) {
     this.selectedCamera = camera;
     this.isSingleView = true;
+  }
+
+  ngAfterViewInit(): void {
+    const webrtc = new SimpleWebRTC({
+      url: 'http://192.168.0.10:8888',
+      // the id/element dom element that will hold "our" video
+      localVideoEl: 'localVideo',
+      // the id/element dom element that will hold remote videos
+      remoteVideosEl: 'remoteVideos',
+      // immediately ask for camera access
+      autoRequestMedia: true
+    });
+    webrtc.on('readyToCall', () => {
+      // you can name it anything
+      webrtc.joinRoom('your awesome room name');
+    });
+    webrtc.startLocalVideo();
   }
 }
