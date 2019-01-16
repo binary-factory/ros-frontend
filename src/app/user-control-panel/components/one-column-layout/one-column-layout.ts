f2cd64eb99abec5cd3906aaeb89f6f443939f03e
Oliver Haase <oliver.koeln@gmail.com>
2019-01-16 19:19:12 +0100

    cleanup one-column-layout-component

--- src/app/user-control-panel/components/one-column-layout/one-column-layout.ts
+++ src/app/user-control-panel/components/one-column-layout/one-column-layout.ts
@@ -1,27 +1,10 @@
-import { Component, OnDestroy } from '@angular/core';
-import { NbThemeService } from '@nebular/theme';
-import { takeWhile } from 'rxjs/operators';
+import { Component } from '@angular/core';
 
-// TODO: move layouts into the framework
 @Component({
   selector: 'ngx-one-column-layout',
   styleUrls: ['./one-column-layout.scss'],
   templateUrl: './one-column-layout.html'
 })
-export class OneColumnLayoutComponent implements OnDestroy {
+export class OneColumnLayoutComponent {
 
-  currentTheme: string;
-  private alive = true;
-
-  constructor(protected themeService: NbThemeService) {
-    this.themeService.getJsTheme()
-      .pipe(takeWhile(() => this.alive))
-      .subscribe(theme => {
-        this.currentTheme = theme.name;
-      });
-  }
-
-  ngOnDestroy() {
-    this.alive = false;
-  }
 }
